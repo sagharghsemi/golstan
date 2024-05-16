1475a287d68e43cd7b61d809fc5d6a3f3b4a23e5
unknown <sajedeeghasemii@gmail.com>
2024-05-16 19:20:12 +0330

    golstan

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.html"],
+  darkMode: "class",
+  theme: {
+    screens: {
+      mobile: "0",
+      tablet: "1024px",
+      desktop: "1536px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
